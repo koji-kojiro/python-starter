bca9ba390d4ded9792abd55bac4b062f40532463
koji-kojiro <kojiro0531@gmail.com>
2017-04-10 22:22:50 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+config = {
+    'name': 'python-starter',
+    'author': 'Kojiro TANI',
+    'author_email': 'kojiro0531@gmail.com',
+    'url': 'https://github.com/koji-kojiro/python-starter',
+    'description': '\'module-starter\' for Python',
+    'long_description': open('README.rst', 'r').read(),
+    'license': 'MIT',
+    'version': '0.1.0',
+    'install_requires': [],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+    ],
+    'packages': find_packages(exclude=['example', ]),
+    'entry_points':
+    '[console_scripts]\npython-starter=python_starter.util:main'
+}
+
+if __name__ == '__main__':
+    setup(**config)
