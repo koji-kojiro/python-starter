ec5a3edefc86d19a1358c56493677219cf805236
koji-kojiro <kojiro0531@gmail.com>
2017-04-12 01:32:20 +0900

    Update CLI

--- python_starter/util.py
+++ python_starter/util.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import argparse
 import subprocess
 from core import Project
@@ -7,27 +8,76 @@ from template.license_tmpl import candidate
 from . import __version__
 
 
-class CLI(object):
+class PythonStarterParser(argparse.ArgumentParser):
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        self.exit(2, 'try \'{} --help\'\n'.format(self.prog))
+
+
+class PythonStarterFormatter(argparse.RawTextHelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings:
+            metavar, = self._metavar_formatter(action, action.dest)(1)
+            return metavar
+        else:
+            parts = []
+            if action.nargs == 0:
+                parts.extend(action.option_strings)
+            else:
+                default = action.dest.upper()
+                args_string = self._format_args(action, default)
+                for option_string in action.option_strings:
+                    parts.append(option_string)
+                parts[-1] += '={}'.format(args_string)
+            return ', '.join(parts)
+
+    def _format_args(self, action, default_metavar):
+        get_metavar = self._metavar_formatter(action, default_metavar)
+        if action.nargs is None:
+            result = '{0:s}'.format(*get_metavar(1))
+        elif action.nargs == argparse.OPTIONAL:
+            result = '{0:s}'.format(*get_metavar(1))
+        elif action.nargs == argparse.ZERO_OR_MORE:
+            result = '[{0:s}...]'.format(*get_metavar(2))
+        elif action.nargs == argparse.ONE_OR_MORE:
+            result = '{0:s}...'.format(*get_metavar(2))
+        elif action.nargs == argparse.REMAINDER:
+            result = '...'
+        elif action.nargs == argparse.PARSER:
+            result = '{0:s}...'.format(*get_metavar(1))
+        else:
+            formats = ['{0:s}' for _ in range(action.nargs)]
+            result = ' '.join(formats).format(*get_metavar(action.nargs))
+        return result
+
+
+class ListLicensesAction(argparse._HelpAction):
+    def __call__(self, parser, *args, **kwargs):
+        print('|'.join(candidate.keys()))
+        parser.exit()
+
+
+class PythonStarter(object):
     def __init__(self):
         try:
             author = subprocess.check_output(
                 'git config --get user.name'.split()).rstrip()
             email = subprocess.check_output(
-                'git config --get user.email'.split()).rstrip()
+                'gita config --get user.email'.split()).rstrip()
             self.git_enabled = True
         except:
-            print('git is not enabled')
             author = ''
             email = ''
             self.git_enabled = False
 
-        self.parser = argparse.ArgumentParser(
+        self.parser = PythonStarterParser(
             prog='python-starter',
-            usage='%(prog)s [options] target',
             add_help=False,
-            formatter_class=argparse.RawTextHelpFormatter)
+            usage='%(prog)s [options] target',
+            formatter_class=PythonStarterFormatter)
         self.parser._optionals.title = 'options'
-        self.parser.add_argument('name', type=str, help=argparse.SUPPRESS)
+        self.parser.add_argument(
+            'name', type=str, metavar='target', help=argparse.SUPPRESS)
         self.parser.add_argument(
             '--author',
             type=str,
@@ -50,15 +100,18 @@ class CLI(object):
             dest='licence',
             nargs='?',
             choices=candidate.keys(),
-            help='license (default: MIT)\n{}'.format(candidate.keys().__str__(
-            )),
+            help='license (default: MIT)',
             default='MIT')
+        self.parser.add_argument(
+            '--list',
+            action=ListLicensesAction,
+            help='list available licenses')
         self.parser.add_argument(
             '--version',
             action='version',
             version='%(prog)s {version}'.format(version=__version__))
         self.parser.add_argument(
-            '--help', action='help', help='show this message and exit')
+            '--help', action='help', help='show this message and exit\n ')
 
     def run(self):
         args = self.parser.parse_args().__dict__
@@ -73,7 +126,7 @@ class CLI(object):
 
 
 def main():
-    cli = CLI()
+    cli = PythonStarter()
     cli.run()
 
 
